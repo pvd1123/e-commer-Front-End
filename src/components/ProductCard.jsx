fbc03f12397521010eb554b2a64efb25b07dbd27
PhatDT2 <phathuynh0106@gmail.com>
2023-07-03 11:14:54 +0700

    product store

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const ProductCard = () =>{
+const ProductCard = (props) =>{
+    const {grid} = props 
+    let location = useLocation();
     return(
     <>
-        <div className="col-3">
+        <div className={` ${location.pathname === "/store" ? `gr-${grid}` : "col-3"} `}>
             <Link className="product-card position-relative">
                 <div className="wishlist-icon position-absolute">
                     <Link>
@@ -26,7 +28,51 @@ const ProductCard = () =>{
                         edit={false}
                         activeColor="#ffd700"
                     />
-                    <p className="price">799$</p>
+                    <p className={` description ${grid === 12 ? "d-block" : "d-none"} `}>
+                        Nhìn tổng thể, đồng hồ thông minh Apple Watch SE 2022 vẫn giữ kiểu thiết kế tương tự như thế hệ tiền nhiệm....
+                    </p>
+                    <b className="price">799$</b>
+                </div>
+                <div className="action-bar position-absolute">
+                    <div className="d-flex flex-column gap-15">
+                        <Link>
+                            <img src="images/prodcompare.svg" alt="compare" />
+                        </Link>
+                        <Link>
+                            <img src="images/view.svg" alt="view" />
+                        </Link>
+                        <Link>
+                            <img src="images/add-cart.svg" alt="addcart" />
+                        </Link>
+                    </div>
+                </div>
+            </Link>
+        </div>
+        <div className={` ${location.pathname === "/store" ? `gr-${grid}` : "col-3"} `}>
+            <Link className="product-card position-relative">
+                <div className="wishlist-icon position-absolute">
+                    <Link>
+                        <img src="images/wish.svg" alt="wishlist" />
+                    </Link>
+                </div>
+                <div className="product-image ">
+                    <img src="images/watch1.jpg" className="img-fluid" alt="product-brand" />
+                    <img src="images/watch-1.avif" className="img-fluid" alt="product-brand" />
+                </div>
+                <div className="product-details">
+                    <h6 className="brand">Apple</h6>
+                    <h5 className="product-title">Apple Watch SE2</h5>
+                    <ReactStars
+                        count={5}
+                        size={24}
+                        value="5"
+                        edit={false}
+                        activeColor="#ffd700"
+                    />
+                    <p className={` description ${grid === 12 ? "d-block" : "d-none"} `}>
+                        Nhìn tổng thể, đồng hồ thông minh Apple Watch SE 2022 vẫn giữ kiểu thiết kế tương tự như thế hệ tiền nhiệm....
+                    </p>
+                    <b className="price">799$</b>
                 </div>
                 <div className="action-bar position-absolute">
                     <div className="d-flex flex-column gap-15">
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta';
 import ReactStars from "react-rating-stars-component";
+import ProductCard from '../components/ProductCard';
 
 const Store = () => {
+    const [grid, setGrid] = useState(4);
   return (
     <>
         <Meta title={"Store"}/>
@@ -171,7 +173,7 @@ const Store = () => {
                         </div>
                     </div>
                     <div className="col-9">
-                        <div className="filter-sort-grid">
+                        <div className="filter-sort-grid mb-4">
                             <div className='d-flex justify-content-between align-items-center'>
                                 <div className='d-flex align-items-center gap-10'>
                                     <p className='mb-0 d-block' style={{width:"100px"}}>Sắp Xếp:</p>
@@ -187,12 +189,18 @@ const Store = () => {
                                 <div className="d-flex align-items-center gap-10">
                                     <p className="total-products mb-0">100 products</p>
                                     <div className='d-flex gap-10 align-items-center grid'>
-                                        <img src="images/gr4.svg" className='d-block img-fluid' alt="grid" />
-                                        <img src="images/gr3.svg" className='d-block img-fluid' alt="grid" />
-                                        <img src="images/gr2.svg" className='d-block img-fluid' alt="grid" />
-                                        <img src="images/gr.svg" className='d-block img-fluid' alt="grid" />
+                                        <img onClick={()=>{setGrid(3);}} src="images/gr4.svg" className='d-block img-fluid' alt="grid" />
+                                        <img onClick={()=>{setGrid(4);}} src="images/gr3.svg" className='d-block img-fluid' alt="grid" />
+                                        <img onClick={()=>{setGrid(6);}} src="images/gr2.svg" className='d-block img-fluid' alt="grid" />
+                                        <img onClick={()=>{setGrid(12);}} src="images/gr.svg" className='d-block img-fluid' alt="grid" />
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="products-list pb-5">
+                            <div className="d-flex gap-10 flex-wrap">
+                                <ProductCard grid={grid} />
+                                <ProductCard grid={grid} />
                             </div>
                         </div>
                     </div>
